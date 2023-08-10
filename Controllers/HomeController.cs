af390d55375b3400c313d58c919520697e31e125
thiag <thiag@LAPTOP-I1GJ06OO>
2023-08-09 21:23:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        
+
+        public IActionResult Privacy()
+        {
+                HomeModel model = new HomeModel();
+
+                home.Nome = "Acaciano neves";
+                home.Email = "acaciano.neves@gmail.com";
+
+            return View(home);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
